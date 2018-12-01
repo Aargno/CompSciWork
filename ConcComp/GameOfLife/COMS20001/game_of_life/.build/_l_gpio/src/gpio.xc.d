156e15c1bfa68bab98d17181fd41f8144ff3f596
Aaron Robey <ar17092@my.bristol.ac.uk>
2018-12-01 12:38:23 +0000

    Added IO functionality, should work the same on any implementation

---
+++ ConcComp/GameOfLife/COMS20001/game_of_life/.build/_l_gpio/src/gpio.xc.d
@@ -0,0 +1,4 @@
+.build/_l_gpio/src/gpio.xc.d .build/_l_gpio/src/gpio.xc.o .build/_l_gpio/src/gpio.xc.pca.xml: /Users/AaronRobey/game-of-life-ws/lib_gpio/src/gpio.xc \
+  /Users/AaronRobey/game-of-life-ws/lib_gpio/api/gpio.h \
+  /Users/AaronRobey/game-of-life-ws/lib_xassert/api/xassert.h
+
--- ConcComp/GameOfLife/COMS20001/game_of_life/.build/src/main.xc.d
+++ ConcComp/GameOfLife/COMS20001/game_of_life/.build/src/main.xc.d
@@ -1,4 +1,4 @@
-.build/src/main.xc.d .build/src/main.xc.o .build/src/main.xc.pca.xml: ./src/main.xc \
+.build/src/main.xc.d .build/src/main.xc.o .build/src/main.xc.pca.xml: ./src/main.xc /Users/AaronRobey/game-of-life-ws/lib_gpio/api/gpio.h \
   ../../COMS20001/game_of_life/src/pgmIO.h \
   ../../COMS20001/lib_i2c/api/i2c.h
 
