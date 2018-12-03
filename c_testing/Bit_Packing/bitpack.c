27dc2a754056892f4fa44e51d9ae44f2edf957e4
Aaron Robey <ar17092@my.bristol.ac.uk>
2018-12-03 09:16:17 +0000

    Added attempt at further compression

--- c_testing/Bit_Packing/bitpack.c
+++ c_testing/Bit_Packing/bitpack.c
@@ -1,24 +1,101 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
+
+int IMWD = 16;
+int IMHT = 16;
 
 typedef unsigned char uchar;
 
-void printBits(uchar c) {
-    for (int i = 7; i >= 0; i--) {
-        printf("%d ", i);
+typedef struct compress_row_data
+{
+    int length;
+    int patternCount[];
+} data;
+
+typedef struct compress_row
+{
+    // data counts;
+    int length;
+    int *patternCount;
+    uchar pattern[];
+} row;
+
+uchar packBit(uchar c, int val, int location);
+int getBit(uchar c, int location);
+
+row * compress(uchar c[IMWD/8])
+{
+    int patternLength = 0; //This will be calculated
+    int lastBit = 2;
+    int bitCount[IMWD]; //Worst case, find a way around?
+    for (int i = 0; i < IMWD; i++) bitCount[i] = 0;
+    int patternLoc = IMWD;
+    int patternX = 0;
+    int get;
+    for (int x = 0; x < IMWD / 8; x++)
+    {
+        for (int i = 7; i >= 0; i--)
+        {
+            get = getBit(c[x], i);
+            if(get != lastBit) {
+                // printf("%d\n", get);
+                // printf("%d\n", patternLength);
+                patternLength++;
+                // printf("%d\n\n", patternLength);
+                lastBit = get;
+            }
+            bitCount[patternLength-2]++;
+        }
     }
-    printf("\n");
-    for (int i = 7; i >= 0; i--) {
+    row *comp = malloc(sizeof(row) + floor(patternLength/8));
+    comp->length = patternLength;
+    comp->patternCount = malloc(sizeof(int) * patternLength);
+    for (int i = 0; i < patternLength; i++) comp->patternCount[i] = bitCount[i];
+    lastBit = 2;
+    for (int x = 0; x < IMWD / 8; x++)
+    {
+        for (int i = 7; i >= 0; i--)
+        {
+            get = getBit(c[x], i);
+            if (get != lastBit) {
+                if (lastBit != 2) {
+                    // printf("%d\n", lastBit);
+                    packBit(comp->pattern[patternX], lastBit, patternLoc % 8);
+                    if (patternLoc % 8 == 0) patternX--;
+                } //packBit(comp->pattern[x], lastBit, patternLoc % 8);
+                lastBit = get;
+                patternLoc--;
+            }
+        }
+    }
+    // printf("%d\n", get);
+    if (get != lastBit) packBit(comp->pattern[patternX], lastBit, patternLoc % 8);
+    return comp;
+}
+
+void printBits(uchar c)
+{
+    // for (int i = 7; i >= 0; i--)
+    // {
+    //     printf("%d ", i);
+    // }
+    // printf("\n");
+    for (int i = 7; i >= 0; i--)
+    {
         int x = (c >> i) & 1;
         printf("%d ", x);
     }
-    printf("\n");
+    // printf("\n");
 }
 
 //These functions will help assue bitpacking happens correctly
-uchar packBit(uchar c, int val, int location) {
-    if (val == 1) c |= 1 << location;
-    else {
+uchar packBit(uchar c, int val, int location)
+{
+    if (val == 1)
+        c |= 1 << location;
+    else
+    {
         char d = 0xFF; //More efficient way for ding this?
         d ^= 1 << location;
         c &= d;
@@ -27,42 +104,65 @@ uchar packBit(uchar c, int val, int location) {
 }
 
 //Gets correct bit
-int getBit(uchar c, int location) {
+int getBit(uchar c, int location)
+{
     int bit = (c >> location) & 1;
-    printf("Bit:%d at Location:%d\n", bit, location);
+    // printf("Bit:%d at Location:%d\n", bit, location);
     return bit;
 }
 
-void emptyChar(uchar val[2][16]) {
-    for (int y = 0; y < 16; y++) for (int x = 0; x < 2; x++) val[x][y] = 0x00;
+void emptyChar(uchar val[16][2])
+{
+    for (int y = 0; y < 16; y++)
+        for (int x = 0; x < 2; x++)
+            val[x][y] = 0x00;
 }
 
-void testArray() {
-    int IMWD = 16; //Test alternate method for accessing array without mod
-    int IMHT = 16;
-    uchar c[2][16];
+void testArray()
+{
+    // int IMWD = 16; //Test alternate method for accessing array without mod
+    // int IMHT = 16;
+    uchar c[16][2];
     emptyChar(c);
-    for (int y = 0; y < IMHT; y++) {
-        for (int i = 0; i < (int)ceil(IMWD/8); i++) printBits(c[i][y]);
-        printf("%d\n", y);
-    }
-    printf("Step\n");
-    for (int y = 0; y < IMHT; y++) { //Works
-        for (int i = 0; i < (int)ceil(IMWD/8); i++)
-            for (int x = 7; x >= 0; x--) {
+    // for (int y = 0; y < IMHT; y++)
+    // {
+    //     for (int i = 0; i < (int)ceil(IMWD / 8); i++)
+    //         printBits(c[y][i]);
+    //     printf("%d\n", y);
+    // }
+    // printf("Step\n");
+    for (int y = 0; y < IMHT; y++)
+    { //Works
+        for (int i = 0; i < (int)ceil(IMWD / 8); i++)
+            for (int x = 7; x >= 0; x--)
+            {
                 //If is just to add some variation in test in implementation would look like:
                 // c[i][y] = packBit(c[i][y], read, x); with read being the channel input 0 or 1
-                if ((x % 2 == 0 && i % 2 == 0) || (x % 2 == 1 && i % 2 == 1)) c[i][y] = packBit(c[i][y], 1, x);
-                else c[i][y] = packBit(c[i][y], 0, x);
+                if ((x % 2 == 0 && i % 2 == 0) || (x % 2 == 1 && i % 2 == 1))
+                    c[y][i] = packBit(c[y][i], 1, x);
+                else
+                    c[y][i] = packBit(c[y][i], 0, x);
             }
     }
-    for (int y = 0; y < IMHT; y++) {
-        for (int i = 0; i < (int)ceil(IMWD/8); i++) printBits(c[i][y]);
-        printf("%d\n", y);
-    }
+    // for (int y = 0; y < IMHT; y++)
+    // {
+    //     for (int i = 0; i < (int)ceil(IMWD / 8); i++)
+    //         printBits(c[y][i]);
+    //     printf("%d\n", y);
+    // }
+    for (int i = 0; i < (int)ceil(IMWD / 8); i++)
+            printBits(c[0][i]);
+    printf("\n");
+    row *comp;
+    comp = compress(c[0]);
+    for (int i = 0; i < 2; i++) printBits(comp->pattern[i]);
+    for (int i = 0; i < 16; i++) printf("\n%d\n",comp->patternCount[i]);
+    printf("\n");
+    printf("%d\n", comp->length);
 }
 
-int main(int n, char **argv) {
+int main(int n, char **argv)
+{
     testArray();
     // uchar c = 0; //Function tests
     // c = packBit(c, 1, 1);
