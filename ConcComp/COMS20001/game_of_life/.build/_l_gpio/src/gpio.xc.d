37b6824c8e699c7eb997fb01e0db434b2df48df3
Aaron Robey <ar17092@my.bristol.ac.uk>
2019-02-21 10:32:43 +0000

    Adding sps work

---
+++ ConcComp/COMS20001/game_of_life/.build/_l_gpio/src/gpio.xc.d
@@ -0,0 +1,5 @@
+.build/_l_gpio/src/gpio.xc.d .build/_l_gpio/src/gpio.xc.o .build/_l_gpio/src/gpio.xc.pca.xml: \
+  ../../COMS20001/lib_gpio/src/gpio.xc \
+  ../../COMS20001/lib_gpio/api/gpio.h \
+  ../../COMS20001/lib_xassert/lib_xassert/api/xassert.h
+
